f124e34cdb9ea4e933b316a740ff54042bdac1b2
VfanLee <fanfanfafafa@gmail.com>
2026-07-08 10:34:32 +0800

    feat: nest

--- apps/api/src/user/user.controller.ts
+++ apps/api/src/user/user.controller.ts
@@ -1,35 +1,26 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Param,
-  Query,
-  Put,
-  Delete,
-} from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user-dto';
-import { UpdateUserDto } from './dto/update-user-dto';
+import { Controller, Get, Post, Body, Param, Query, Put, Delete } from '@nestjs/common'
+import { CreateUserDto } from './dto/create-user-dto'
+import { UpdateUserDto } from './dto/update-user-dto'
 
 const users = [
   { id: 1, name: 'John Doe' },
   { id: 2, name: 'Adam Smith' },
-];
+]
 
 @Controller('user')
 export class UserController {
   @Get() // GET /user
   getUsers(@Query('name') name?: string) {
     if (!name) {
-      return users;
+      return users
     }
-    const keyword = name.toLowerCase();
-    return users.filter((user) => user.name.toLowerCase().includes(keyword));
+    const keyword = name.toLowerCase()
+    return users.filter((user) => user.name.toLowerCase().includes(keyword))
   }
 
   @Get(':id') // GET /user/:id
   getUser(@Param('id') id: string) {
-    return users.find((user) => user.id === parseInt(id));
+    return users.find((user) => user.id === parseInt(id))
   }
 
   @Post() // POST /user
@@ -37,7 +28,7 @@ export class UserController {
     return {
       data: createUserDto,
       message: 'User created successfully',
-    };
+    }
   }
 
   @Put(':id') // PUT /user/:id
@@ -45,7 +36,7 @@ export class UserController {
     return {
       data: { id, ...updateUserDto },
       message: 'User updated successfully',
-    };
+    }
   }
 
   @Delete(':id') // DELETE /user/:id
@@ -53,6 +44,6 @@ export class UserController {
     return {
       data: { id },
       message: 'User deleted successfully',
-    };
+    }
   }
 }
